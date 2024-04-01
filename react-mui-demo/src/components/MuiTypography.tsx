bfb359d82a4f403285a13945da1bc9bcef9a63b9
Marius Chisa <mchisa@idirect.net>
2024-04-01 21:28:11 +0200

    Added some code

---
+++ react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,21 @@
+import { Typography } from "@mui/material"
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">subtitle 1</Typography>
+      <Typography variant="subtitle2">subtitle 2</Typography>
+
+      <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci facere, ipsa delectus ea a praesentium aperiam aliquam et quidem minus molestias voluptatum quo fuga minima rerum reiciendis rem commodi mollitia?</Typography>
+      <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores saepe sapiente recusandae dolorum. Sed, optio! Vel similique ex, quo assumenda blanditiis laborum veritatis, rem dignissimos quisquam illum sed debitis tenetur?</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
